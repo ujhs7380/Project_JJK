883bda6716affb175c486f55dc70355cf50b472b
jkwchunjae <jkwchunjae@daum.net>
2015-07-23 19:55:13 +0900

    Nancy 추가

---
+++ Project_JJK/Project_JJK/Modules/HomeModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using System.Dynamic;
+
+namespace Project_JJK.Modules
+{
+	public class HomeModule : NancyModule
+	{
+		/// <summary>
+		/// http://localhost 로 들어온 명령을 처리한다.
+		/// Get["/"]은 기본 도메인 주소일 때 호출되는 함수.
+		/// View["home"] 이 의미하는 것은 Views 폴더에서 home.cshtml 을 찾아서 보여주겠다는 뜻.
+		/// </summary>
+		public HomeModule()
+		{
+			//Get["/"] = _ => View["home"]; // 이렇게 써도 되지만 View 하기 전에 다른 로직들이 있다면 아래처럼 해야겠지.
+
+			Get["/"] = _ =>
+			{
+				return View["home"];
+			};
+		}
+	}
+}
--- Project_JJK/Project_JJK/Program.cs
+++ Project_JJK/Project_JJK/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nancy.Hosting.Self;
 
 namespace Project_JJK
 {
@@ -10,6 +11,15 @@ namespace Project_JJK
     {
         static void Main(string[] args)
         {
+			// Nancy는 WebServer!!
+			// 음 그러니까 웹로 접속 할 수 있는 뭔가를 만들겠다는 뜻!
+			// https://ko.wikipedia.org/wiki/웹_서버
+			using (var host = new NancyHost(new Uri("http://localhost:80")))
+			{
+				host.Start(); // 시작한다.
+				Console.WriteLine("Start server.");
+				Console.ReadLine(); // 이거 안해주면 host가 그냥 끝나버린다.
+			}
         }
     }
 }
