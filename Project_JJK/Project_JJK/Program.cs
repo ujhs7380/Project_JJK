7a5d4652777522e2138a8d0b4d2504190e135d4b
kown8447 <kown8447@naver.com>
2015-08-07 18:01:33 +0900

    sql C# connection TEST
    연결 테스트

--- Project_JJK/Project_JJK/Program.cs
+++ Project_JJK/Project_JJK/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Nancy.Hosting.Self;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace Project_JJK
 {
@@ -11,7 +13,9 @@ namespace Project_JJK
     {
         static void Main(string[] args)
         {
-			// Nancy는 WebServer!!
+            Insert();
+
+  			// Nancy는 WebServer!!
 			// 음 그러니까 웹로 접속 할 수 있는 뭔가를 만들겠다는 뜻!
 			// https://ko.wikipedia.org/wiki/웹_서버
 			using (var host = new NancyHost(new Uri("http://localhost:80")))
@@ -20,6 +24,24 @@ namespace Project_JJK
 				Console.WriteLine("Start server.");
 				Console.ReadLine(); // 이거 안해주면 host가 그냥 끝나버린다.
 			}
+        }
+
+        public static void Insert()
+        {
+            var connBuilder = new MySqlConnectionStringBuilder();
+            connBuilder.Server = "hellojkw.com";
+            connBuilder.UserID = "kown8447";
+            connBuilder.Password = "kown8447";
+            connBuilder.Database = "Project_JJK";
+            connBuilder.CharacterSet = "utf8";
+            string strconn = connBuilder.ToString();
+
+            using (MySqlConnection conn = new MySqlConnection(strconn))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO test1 VALUES (20, '뭐')", conn);
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
